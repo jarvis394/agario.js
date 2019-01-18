2862f0e29ee0c03cb595168c0685400ae276d590
jarvis394 <vlad.ekushev@mail.ru>
2019-01-18 22:30:38 +0300

    Update

--- public/js/Grid.js
+++ public/js/Grid.js
@@ -12,12 +12,12 @@ class Grid {
     stroke(100, 100, 150, 32)
     strokeWeight(1)
 
-    for (let i = -(FIELD_SIZE / this.size); i < 2 * FIELD_SIZE / this.size; i++) {
-      line(-FIELD_SIZE, this.size * i, FIELD_SIZE * 2, this.size * i)
+    for (let i = 0; i < FIELD_SIZE / this.size; i++) {
+      line(0, this.size * i, FIELD_SIZE, this.size * i)
     }
 
-    for (let i = -(FIELD_SIZE / this.size); i < 2 * FIELD_SIZE / this.size; i++) {
-      line(this.size * i, -FIELD_SIZE, this.size * i, FIELD_SIZE * 2)
+    for (let i = 0; i < FIELD_SIZE / this.size; i++) {
+      line(this.size * i, 0, this.size * i, FIELD_SIZE)
     }
   }
 }
